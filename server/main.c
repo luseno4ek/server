b49d91cc1f0d879c9d1b3ce6e22368d6cfbdc0ae
luseno4ek <olindyv@mail.ru>
2020-12-06 22:35:57 +0300

    +InitServ

--- server/main.c
+++ server/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 void ReadPort(int* port, int argc, char* argv[]) {
     if(argc == 1) {
@@ -12,11 +15,32 @@ void ReadPort(int* port, int argc, char* argv[]) {
             *port = *port * 10 + argv[1][i] - 48;
         }
     }
-    
+}
+
+void InitServer(int* socket_serv, int port) {
+    *socket_serv = -1;
+    while(*socket_serv == -1) {
+        *socket_serv = socket(AF_INET, SOCK_STREAM, 0);
+    }
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+    int bind_res = bind(*socket_serv, (struct sockaddr*) &addr, sizeof(addr));
+    if(bind_res == -1) {
+        printf("ERROR: Fail to bind socket, try again\n");
+        _exit(2);
+    }
+    int ls = listen(*socket_serv, 5);
+    if(ls == -1) {
+        printf("ERROR: Fail to listen socket, try again\n");
+        _exit(2);
+    }
 }
 
 int main(int argc, char* argv[]) {
     int port = 0;
     ReadPort(&port, argc, argv);
-    printf("port = %d\n", port);
+    int socket_serv;
+    InitServer(&socket_serv, port);
 }
