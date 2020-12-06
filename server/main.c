f87fd81957a760f2a95536c821546f4f5aa29bcf
luseno4ek <olindyv@mail.ru>
2020-12-06 21:44:08 +0300

    Check

--- server/main.c
+++ server/main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-void ReadPort(int* port, int argc, char** argv) {
+void ReadPort(int* port, int argc, char* argv[]) {
     if(argc == 1) {
         printf("Please run again with port in arguments\n");
+        _exit(1);
     } else {
-        char* temp = argv[1];
-        for(int i = 0; temp[i] != ''; i++) {
-            
+        for(int i = 0; i < strlen(argv[1]); i++) {
+            *port = *port * 10 + argv[1][i] - 48;
         }
     }
     
 }
 
 int main(int argc, char* argv[]) {
-    int port;
+    int port = 0;
     ReadPort(&port, argc, argv);
+    printf("port = %d\n", port);
 }
